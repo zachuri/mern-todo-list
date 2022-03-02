9bb4473df4ddcdbb2ccfcad10e64921ee6c6e10b
zachuri <punsalang.zachary@gmail.com>
2022-03-02 03:19:41 -0800

    made Project item pressable/ display list of task list

--- mernTodoList/components/ProjectItem/index.tsx
+++ mernTodoList/components/ProjectItem/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from './styles';
@@ -12,8 +12,12 @@ interface ProjectItemProps {
 }
 
 const ProjectItem = ({ project }: ProjectItemProps) => {
+	const onPress = () => {
+		console.warn(`open project ${project.title}`);
+	};
+
 	return (
-		<View style={styles.root}>
+		<Pressable onPress={onPress} style={styles.root}>
 			{/* Project/Task List  */}
 			<View style={styles.iconContainer}>
 				<MaterialCommunityIcons
@@ -26,7 +30,7 @@ const ProjectItem = ({ project }: ProjectItemProps) => {
 				<Text style={styles.title}>{project.title}</Text>
 				<Text style={styles.time}>{project.createdAt}</Text>
 			</View>
-		</View>
+		</Pressable>
 	);
 };
 
