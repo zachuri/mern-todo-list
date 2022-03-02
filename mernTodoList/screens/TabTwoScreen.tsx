81c56b78ba14c7517f3738494c49b089b0738d35
zachuri <punsalang.zachary@gmail.com>
2022-03-02 02:57:43 -0800

    setup TaskList page/ need to make into a Component

--- mernTodoList/screens/TabTwoScreen.tsx
+++ mernTodoList/screens/TabTwoScreen.tsx
@@ -1,31 +1,54 @@
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{/* Project/Task List  */}
+			<View style={styles.root}>
+				<View style={styles.iconContainer}>
+					<MaterialCommunityIcons
+						name="file-outline"
+						size={24}
+						color="grey"
+					/>
+				</View>
+				<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+					<Text style={styles.title}>Title</Text>
+					<Text style={styles.time}>2d</Text>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	root: {
+		flexDirection: 'row',
+		width: '100%',
+		padding: 10,
+	},
+	iconContainer: {
+		width: 40,
+		height: 40,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 10,
+		backgroundColor: '#404040',
+		marginRight: 10,
+	},
+	title: {
+		fontSize: 20,
+		marginRight: 5,
+	},
+	time: {
+		color: 'darkgrey',
+	},
 });
